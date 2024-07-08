7bee70d79ff96eae73f7020c36807662adb6cbba
Cesar Mora <ingeniotekcol@gmail.com>
2024-07-08 12:11:04 -0500

    MVT for coffee_shop, model, view, form, template connection and page render for product creation.

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,19 @@
 from django import forms
+from .models import Product
+
 
 class ProductForm(forms.Form):
-    name = forms.TextField(max_length=200, label="Nombre")
-    description = forms.TextField(max_length=300, label="Descripción")
+    name = forms.CharField(max_length=200, label="Nombre")
+    description = forms.CharField(max_length=300, label="Descripción")
     price = forms.DecimalField(max_digits=10, decimal_places=2, label="Precio")
-    available=forms.BooleanField(initial=True, label="Disponble", required=False)
+    available = forms.BooleanField(initial=True, label="Disponble", required=False)
     photo = forms.ImageField(label="Foto", required=False)
+
+    def save(self):
+        Product.objects.create(
+            name=self.cleaned_data["name"],
+            description=self.cleaned_data["description"],
+            price=self.cleaned_data["price"],
+            available=self.cleaned_data["available"],
+            photo=self.cleaned_data["photo"],
+        )
